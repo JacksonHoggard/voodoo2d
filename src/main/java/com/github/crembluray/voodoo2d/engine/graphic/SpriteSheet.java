f13e57e17c1f9967c70b45646fb4083c1eb631a2
CremBluRay <jhoggard2003@gmail.com>
2020-05-02 23:16:54 -0500

    add texture loading method to SpriteSheet.java

--- src/main/java/com/github/crembluray/voodoo2d/engine/graphic/SpriteSheet.java
+++ src/main/java/com/github/crembluray/voodoo2d/engine/graphic/SpriteSheet.java
@@ -40,6 +40,37 @@ public class SpriteSheet {
         }
     }
 
+    public SpriteSheet(String filename) {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File(filename));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        int rows = img.getHeight();
+        int cols = img.getWidth();
+
+        BufferedImage[] sprites = new BufferedImage[1];
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                sprites[(i * cols) + j] = img.getSubimage(
+                        j * img.getWidth(),
+                        i * img.getHeight(),
+                        img.getWidth(),
+                        img.getHeight()
+                );
+            }
+        }
+
+        try {
+            textures = Texture.loadTexture(sprites);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public Texture[] getTextures() {
         return textures;
     }
